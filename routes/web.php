f5d96f9ce2507a447d55a8e20e1daa5fed558f8e
Eryk Sikora <e.sikora95@gmail.com>
2020-09-22 20:26:13 +0200

    routing and building templates

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/posts');
 });
+
+Route::get('/about', function () {
+    return view('pages/about');
+})->name('about');
