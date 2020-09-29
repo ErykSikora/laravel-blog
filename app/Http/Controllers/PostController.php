cdeb8a182721d8895370e4d6622379e80f69ebb6
Eryk Sikora <e.sikora95@gmail.com>
2020-09-29 17:38:11 +0200

    0.1.1 routing parameters

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,4 +17,18 @@ class PostController extends Controller
             'posts' => $posts // można zastosować funkcję compact('posts')
         ]);
     }
+
+    public function show($id) {
+        $post = Post::findOrFail($id);
+
+        // $post = Post::find($id);
+        // if (is_null($post)) return abort(404); // abort zwraca błąd laravel
+
+        dd($post);
+
+        return view('pages/post', [
+            'post' => $post
+        ]);
+    }
+
 }
