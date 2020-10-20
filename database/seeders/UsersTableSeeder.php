39e99f4b9462cc909ebc049bcae97c4256dee792
Eryk Sikora <e.sikora95@gmail.com>
2020-10-20 16:24:39 +0200

    0.2.3 user factory fix

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        //PostFactory.php
+
+        $factory = User::factory();
+        $factory->admin()->create(); // generates user (type admin)
+        $factory->count(3)->create(); // generates three regular users
+
+    }
+}
